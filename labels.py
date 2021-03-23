f552af56ed2c5dd8708c5c1899625d4efa622398
Samuel Huang <58419736+kommunium@users.noreply.github.com>
2021-03-23 19:59:20 +0800

    Update labels

--- labels.py
+++ labels.py
@@ -1,7 +1,25 @@
-path = '../../data/VOC2007/'
-with open(path + 'label_train.txt', 'w') as fw:
+# %%
+path = "labels/"
+with open('label_train.txt', 'w') as fw:
     for i in range(1, 201):
-        file_name = path + 'labels/' + str(i).rjust(7, '0') + '.txt'
+        file_name = str(i).rjust(7, '0') + '.txt'
+        data = file_name + ' '
+        with open(path + file_name, 'r') as fr:
+            for line in fr.readlines():
+                obj = line.strip().split(' ')
+                c = obj[0]
+                obj = list(map(float, obj[1:]))
+                x1 = round((obj[0] - .5 * obj[2]) * 640)
+                y1 = round((obj[1] - .5 * obj[3]) * 480)
+                x2 = round((obj[0] + .5 * obj[2]) * 640)
+                y2 = round((obj[1] + .5 * obj[3]) * 480)
+                data += str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + c + ' '
+            fw.write(data.strip() + '\n')
+
+# %%
+with open('label_train.txt', 'w') as fw:
+    for i in range(1, 201):
+        file_name = path + str(i).rjust(7, '0') + '.txt'
         with open(file_name, 'r') as fr:
             for line in fr.readlines():
                 fw.write(line.strip() + ' ')
