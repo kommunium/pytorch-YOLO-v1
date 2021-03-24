c3aa7f6e2ed24aaff77db4776ee81dfd5e03a016
Samuel Huang <58419736+kommunium@users.noreply.github.com>
2021-03-24 15:25:35 +0800

    Update labels.py

--- labels.py
+++ labels.py
@@ -2,9 +2,9 @@
 path = "labels/"
 with open('label_train.txt', 'w') as fw:
     for i in range(1, 201):
-        file_name = str(i).rjust(7, '0') + '.txt'
-        data = file_name + ' '
-        with open(path + file_name, 'r') as fr:
+        file_name = str(i).rjust(7, '0')
+        data = file_name + '.jpg '
+        with open(path + file_name + '.txt', 'r') as fr:
             for line in fr.readlines():
                 obj = line.strip().split(' ')
                 c = obj[0]
@@ -13,7 +13,7 @@ with open('label_train.txt', 'w') as fw:
                 y1 = round((obj[1] - .5 * obj[3]) * 480)
                 x2 = round((obj[0] + .5 * obj[2]) * 640)
                 y2 = round((obj[1] + .5 * obj[3]) * 480)
-                data += str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + c + ' '
+                data += '{} {} {} {} {} '.format(x1, y1, x2, y2, c)
             fw.write(data.strip() + '\n')
 
 # %%
